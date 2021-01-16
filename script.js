f634ed8e6580601343b9e108814c44870238326a
Ankit Verma <ankitiitian24@gmail.com>
2021-01-17 00:44:50 +0530

    added clock file

--- script.js
+++ script.js
@@ -194,3 +194,24 @@ for(var i =0;i<number.length;i++){
 
 /**********************************************************************************************************/
 /**********************************************************************************************************/
+
+/***********************************************************************************
+ No 5
+ JavaScript Code for
+ Analog Clock (clock.html)
+ ***********************************************************************************/
+ const deg=6;
+ const hr=document.querySelector("#hr");
+ const mn=document.querySelector("#mn");
+ const sec=document.querySelector("#sec");
+
+ setInterval(()=>{
+	 let day= new Date();
+	 var hh = day.getHours()*30;
+	 let mm = day.getMinutes()*deg;
+	 let ss = day.getSeconds()*deg;
+	 hr.style.transform = "rotateZ(${hh+(mm/12)}deg)";
+	 mn.style.transform = "rotateZ(${mm}deg)";
+	 sec.style.transform = "rotateZ(${ss}deg)";
+ });
+ 
