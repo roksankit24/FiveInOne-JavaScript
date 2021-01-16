7924cef809491722a2b2aeecfdba4607b8d847eb
Ankit Verma <ankitiitian24@gmail.com>
2021-01-16 16:06:53 +0530

    corrent responsiveness

--- script.js
+++ script.js
@@ -1,3 +1,118 @@
+/*********No. 1*************************************************************
+ JAVASCRIPT CODE FOR 
+ GRADE CALCULATOR  (index.html) 
+ FUNCTION NAME => calcy()
+***************************************************************************/
+const calcy = () =>{
+	let core1 = document.getElementById('core1').value;
+	let core2 = document.getElementById('core2').value;
+	let ge = document.getElementById('ge').value;
+	let sec = document.getElementById('sec').value;
+	let grades = "";
+  
+  
+	let totalGrades = parseFloat(core1) + parseFloat(core2) + parseFloat(ge) + parseFloat(sec);
+  
+  
+	let perc = (totalGrades/400) * 100;
+  
+  
+	debugger;
+  
+	if(perc <= 100  && perc >= 80){
+	  grades = 'A';
+	}else if(perc <= 79  && perc >= 60){
+	   grades = 'B';
+	}else if(perc <= 59  && perc >= 40){
+	   grades = 'C';
+	}else{
+	   grades = 'F';
+	}
+  
+	if(perc >= 39.5){
+	  document.getElementById('showData').innerHTML = ` Out of 400 your total is  ${totalGrades} and percentage is ${perc}%. <br> Your grade is ${grades}. You are Pass. `
+	}
+	else{
+	  document.getElementById('showData').innerHTML = ` Out of 400 your total is  ${totalGrades} and percentage is ${perc}%. <br> Your grade is ${grades}. You are Fail. `
+	}
+  
+	
+  }
+
+/************************************************************************************************************************************/
+/************************************************************************************************************************************/
+
+/***********************************************************************************
+ No 2
+ JavaScript Code for
+ Body Mass Index (bmi.html)
+ ***********************************************************************************/
+const bmi=()=>{
+	let mname = document.getElementById('name').value;
+	let feet = document.getElementById('feet').value;
+	let inch = document.getElementById('inch').value;
+	let wt = document.getElementById('wt').value;
+   
+	// Merge Feet and Inches Values in one
+	let merge = parseFloat(feet) + "." + parseFloat(inch);
+	
+	// Convert Height into Metres.
+	let height= merge/3.2808;
+	
+	// Calculating BMI     
+	let index= parseFloat(wt) / (height*height);
+	
+   
+	debugger;
+	 
+   if(index < 18.50){
+	  
+	  document.getElementById('showData').innerHTML = " Hi "+(mname)+", Your Body Mass Index (BMI) is "+ (index.toFixed(2)) +"<br>Your Value is Under Weight. "
+	}
+	else if ((index >= 18.50) && (index <=24.90)){
+	 
+	  document.getElementById('showData').innerHTML = " Hi "+(mname)+", Your Body Mass Index (BMI) is "+ (index.toFixed(2)) +"<br>Your Value is Normal Weight. "
+	}
+	else if ((index >= 25.00) && (index <=29.90)){
+   
+	  document.getElementById('showData').innerHTML = " Hi "+(mname)+", Your Body Mass Index (BMI) is "+ (index.toFixed(2)) +"<br>Your Value is Over Weight. "
+	}
+	else if (index >= 30.00){
+	  
+	  document.getElementById('showData').innerHTML = " Hi "+(mname)+", Your Body Mass Index (BMI) is "+ (index.toFixed(2)) +"<br>Your Value is Obesity . "
+	}   
+  }
+
+/************************************************************************************************/
+/************************************************************************************************/
+
+/***********************************************************************************
+ No 3
+ JavaScript Code for
+ BULB (bulb.html)
+ ***********************************************************************************/
+function light(){
+
+	var bid = document.getElementById('bulb');
+	 if (bid.src.match("On.png")){
+	   bid.src="Off.png";
+	 }
+	 else{
+	   bid.src="On.png";
+	 }
+
+  }
+
+/************************************************************************************************/
+/************************************************************************************************/
+
+/***********************************************************************************
+ No 4
+ JavaScript Code for
+ Calculator (calculator.html)
+ ***********************************************************************************/
+
+
 function getHistory(){
 	return document.getElementById("history-value").innerText;
 }
@@ -76,3 +191,6 @@ for(var i =0;i<number.length;i++){
 		}
 	});
 }
+
+/**********************************************************************************************************/
+/**********************************************************************************************************/
